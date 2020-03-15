eddfaefa1a4e01a969b17f09ffb5488be3bd10ba
Ravindu W <ravindu@serendibsystems.com>
2020-03-15 14:28:40 +1100

    Initial Commit

---
+++ Asset Parser/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Asset Parser
+//
+//  Created by Spritzer App on 15/3/20.
+//  Copyright © 2020 Spritzer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
